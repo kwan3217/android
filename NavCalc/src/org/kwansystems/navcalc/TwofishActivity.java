88bcb1fdaa75fb21ad648fd19c4fd2f4ff3d8285
Mio <jeppesen@lasp.colorado.edu>
2014-04-16 18:17:46 -0600

    Added safety feature

--- NavCalc/src/org/kwansystems/navcalc/TwofishActivity.java
+++ NavCalc/src/org/kwansystems/navcalc/TwofishActivity.java
@@ -14,34 +14,54 @@ import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.Toast;
 
 public class TwofishActivity extends Activity {
+  @Override
+  public void onTrimMemory(int level) {
+    // TODO Auto-generated method stub
+    super.onTrimMemory(level);
+    if(level==TRIM_MEMORY_UI_HIDDEN) {
+      TwofishActivity.this.finish();
+    }
+  }
   protected void save() {
     final AlertDialog.Builder alert = new AlertDialog.Builder(this);
-    final EditText input = new EditText(this);
-
-    input.setInputType(InputType.TYPE_CLASS_TEXT| InputType.TYPE_TEXT_VARIATION_PASSWORD);
-
-    alert.setView(input);    //edit text added to alert
+    final EditText input1 = new EditText(this);
+    final EditText input2 = new EditText(this);
+    final LinearLayout view=new LinearLayout(getApplicationContext());
+    view.setOrientation(1);
+    input1.setInputType(InputType.TYPE_CLASS_TEXT| InputType.TYPE_TEXT_VARIATION_PASSWORD);
+    input2.setInputType(InputType.TYPE_CLASS_TEXT| InputType.TYPE_TEXT_VARIATION_PASSWORD);
+    view.addView(input1);
+    view.addView(input2);
+    alert.setView(view);    //edit text added to alert
+//    alert.setView(input2);    //edit text added to alert
     alert.setTitle("Please enter passphrase");   //title setted
     alert.setCancelable(true);
     alert.setPositiveButton("OK",new DialogInterface.OnClickListener() {
       public void onClick(DialogInterface dialog,int id) {
         // if this button is clicked, close
         // current activity
-        String passphrase=input.getText().toString();
-        EditText TwofishNotes=(EditText) findViewById(R.id.txtTwofishNotes);
-        SharedPreferences settings = getSharedPreferences("TwofishNote", 0);
-        SharedPreferences.Editor edit=settings.edit();
-        try {
-          edit.putString("ciphertext", KwanCryptWrapper.encrypt(passphrase,TwofishNotes.getText().toString()));
-        } catch (InvalidKeyException e) {
-          // TODO Auto-generated catch block
-          Toast.makeText(getApplicationContext(), "Problem with save", Toast.LENGTH_SHORT).show();
+        String passphrase=input1.getText().toString();
+        String passphrase2=input2.getText().toString();
+        if (passphrase.equals(passphrase2)) {
+          EditText TwofishNotes=(EditText) findViewById(R.id.txtTwofishNotes);
+          SharedPreferences settings = getSharedPreferences("TwofishNote", 0);
+          SharedPreferences.Editor edit=settings.edit();
+          try {
+            edit.putString("ciphertext", KwanCryptWrapper.encrypt(passphrase,TwofishNotes.getText().toString()));
+          } catch (InvalidKeyException e) {
+            // TODO Auto-generated catch block
+            Toast.makeText(getApplicationContext(), "Problem with save, nothing changed", Toast.LENGTH_SHORT).show();
+          }
+          edit.commit();
+          TwofishActivity.this.finish();
+        } else {
+          Toast.makeText(getApplicationContext(), "Passwords don't match, nothing changed", Toast.LENGTH_SHORT).show();
         }
-        edit.commit();
-        TwofishActivity.this.finish();
       }
     });
     alert.setNegativeButton("Cancel",new DialogInterface.OnClickListener() {

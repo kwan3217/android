1993595cb952e01326a14ba1223092b6d40d4dcd
kwan3217 <kwan3217@gmail.com>
2020-01-28 11:10:24 -0700

    Rebuilt project structure in Android Studio, source code only copied

---
+++ Paradox3/app/src/main/java/org/kwansystems/paradox3/MainActivity.java
@@ -0,0 +1,82 @@
+package org.kwansystems.paradox3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+    public static boolean check(int[] Clock, int Start, int Branches, int[] Path) {
+        boolean[] Mark=new boolean[Clock.length];
+        int here=Start;
+        for (int i=0;i<Clock.length;i++) {
+            if(Mark[here]) return false;
+            Path[i]=here;
+            Mark[here]=true;
+            int jump=Clock[here];
+            if((Branches & 1)!=0) jump=-jump;
+            here+=jump;
+            if(here<0) here+=Clock.length;
+            here=here % Clock.length;
+            Branches=Branches/2;
+        }
+        return true;
+    }
+
+    //		  public static void main(String[] args) {
+//		    int[] Clock=new int[] {3,5,3,5,2,1,5,3,2,3,3};
+//		  }
+    EditText txtNumbers;
+    TextView txtSolution;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_paradox3);
+        txtNumbers=(EditText)findViewById(R.id.txtNumbers);
+        txtSolution=(TextView)findViewById(R.id.txtSolution);
+    }
+
+    public void clickNum(View view) {
+        txtNumbers.setText(txtNumbers.getText().toString()+((Button)view).getText().toString());
+    }
+
+    public void clickSolve(View view) {
+        String S="";
+        String ClockString=txtNumbers.getText().toString();
+        int[] Clock=new int[ClockString.length()];
+        for(int i=0;i<Clock.length;i++) Clock[i]=ClockString.codePointAt(i)-48;
+        int[] Path=new int[Clock.length];
+        int acc=0;
+        boolean found=false;
+        int start=0;
+        while(start<Clock.length && !found) {
+            int branches=0;
+            while(branches<(1 << Clock.length) && !found) {
+                acc++;
+                if(check(Clock,start,branches,Path)) {
+                    found=true;
+                    S=S+"Found a path\n";
+                    for(int step=0;step<Path.length;step++) {
+                        S=S+String.format("Step %2d: %2d (%2d)\n",step+1,Path[step],Clock[Path[step]]);
+                    }
+                }
+                branches++;
+            }
+            start++;
+        }
+        if(!found) {
+            S=S+"No path found\n";
+        }
+        S=S+String.format("Checked %d possibilities\n",acc);
+        txtSolution.setText(S);
+    }
+
+    public void clickClear(View view) {
+        txtNumbers.setText("");
+    }
+}
